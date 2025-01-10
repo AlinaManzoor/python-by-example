fe85d749ddc1bf907f4a1faf3b860536c92198c4
AlinaManzoor <alina83318@gmail.com>
2025-01-10 12:16:04 +0500

    hours , minutes , seconds

---
+++ example06.py
@@ -0,0 +1,5 @@
+days = int (input (" enter the humber of days : " , ))
+
+print("Hours in the days is  : " ,days*24)
+print("Minutes in the days is : " , days*24*60)
+print("Seconds in the days is : " , days*24*60*60)
