77f10da016ff43ccd1a268314cdae73c3883a551
AlinaManzoor <alina83318@gmail.com>
2025-01-11 17:34:54 +0500

    (if/else) b/w greater nums

---
+++ example09.py
@@ -0,0 +1,7 @@
+num1 = int(input("enter first the no : " , ))
+num2 = int(input("enter second no : " , ))
+
+if num1 > num2 :
+    print(num2 ," is smaller than " , num1 )
+else:
+    print(num1, " is smaller than " , num2)
