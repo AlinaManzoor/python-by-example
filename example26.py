d416bd1f0aad09904fa995b1c88a1337f83e86b5
AlinaManzoor <alina83318@gmail.com>
2025-01-12 12:39:17 +0500

    divison and remainder

---
+++ example26.py
@@ -0,0 +1,9 @@
+import math
+ 
+num1 = int(input("enter the nominator  : " , ))
+num2 = int(input("enter the denominator: " , ))
+
+div = num1 // num2
+rem = num1 % num2
+
+print(num1 , "divided by" , num2, "is" , div, "with" , rem , "remaining")
