62160acd32e55bd9c7428ee5acb91364248d6506
AlinaManzoor <alina83318@gmail.com>
2025-01-12 17:23:20 +0500

    while loop

---
+++ example38.py
@@ -0,0 +1,8 @@
+
+#num = int(input("enter the num : " , ))
+while num < 5:
+    num = int(input("enter the num : " , ))
+    
+print("The last number you entered was a ", num , " and stop the program")
+
+
