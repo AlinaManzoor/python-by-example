b991038b6735c860b1ba50621a81d9e14c7afd8d
AlinaManzoor <alina83318@gmail.com>
2025-01-12 12:32:21 +0500

    area and volume

---
+++ example25.py
@@ -0,0 +1,10 @@
+import math
+
+radius = int(input("enter the radius : " ,))
+dept = int(input ("enter the depth of cylinder : " , ))
+
+area = math.pi *radius
+volume = area*dept
+
+print("Area of circle : "  , area)
+print("volume of circle : "  , volume)
