30d38d2d63a3319d4e33acaa4108ab18480d363a
AlinaManzoor <alina83318@gmail.com>
2025-01-12 14:14:00 +0500

    repeation of nums in 5 time then add

--- example35.py
+++ example35.py
@@ -3,7 +3,7 @@ total = 0
 
 for i in range( 0 , 5):
     num = int(input("enter the numbers : " , ))
-    decision = input("Do you want that number included (y/n): " , )
+    decision = input("Do you want that number included? (y/n): " , )
     if decision == "y":
         total = total + num
 print(total) 
