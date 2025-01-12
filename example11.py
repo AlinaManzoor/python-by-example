90c2dd6da0997f4713a70812f1067e1180283066
AlinaManzoor <alina83318@gmail.com>
2025-01-12 08:13:21 +0500

    choose red color

--- example11.py
+++ example11.py
@@ -1,8 +1,6 @@
 color = input("enter the color : " , )
-color1 = "red"
-color2 = "RED"
 
-if color == color1 or color == color2 :
+if color == "red" or color == "RED" or color == "Red" :
     print("I like red color")
 else:
     print("I prefer red")
