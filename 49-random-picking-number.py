60c7e1f00757fa8ba3fc0ce28e3eee1581421db5
AlinaManzoor <alina83318@gmail.com>
2025-01-13 12:43:26 +0500

    refactor: restructure files

---
+++ 49-random-picking-number.py
@@ -0,0 +1,9 @@
+import random
+
+rand = random.randint( 1, 10 )
+correct = False 
+
+while correct == False:
+    num = int(input("enter a number : " , ))
+    if num == correct:
+        correct = True
