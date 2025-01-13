4e008ae41edf56ad6a0f1057f1ac01019ba61acb
AlinaManzoor <alina83318@gmail.com>
2025-01-13 18:03:59 +0500

    refactor: restructue files

--- 53-Draw-circle.py
+++ 53-Draw-circle.py
@@ -1,35 +1,8 @@
 import turtle
 
-turtle.color("green" , "pink")
-turtle.begin_fill()
-
-for i in range(0,4):
-   turtle.forward(100)
-   turtle.left(90)
-turtle.penup()
-turtle.end_fill()
-turtle.forward(100)
-
-turtle.pendown()
-turtle.color("green" , "pink")
-turtle.begin_fill()
-
-for i in range(0,4):
-   turtle.forward(100)
-   turtle.left(90)
-turtle.end_fill()
-turtle.penup()
-turtle.forward(100)
-
-turtle.pendown()
-turtle.color("green" , "pink")
-turtle.begin_fill()
-
-for i in range(0,4):
-    turtle.forward(100)
-    turtle.left(90)
-turtle.penup()
-turtle.end_fill()
-turtle.forward(100)
+for i in range(0,360):
+    turtle.forward(1)
+    turtle.left(1)
+    
 
 turtle.exitonclick()
---
+++ 56-countries-name-tuple().py
@@ -0,0 +1,7 @@
+
+country_tuple = ("Pakistan" , "India" , "Afghanistan" , "Bangladesh" , "China")
+
+print(country_tuple)
+
+countries = input("enter the country name : " , )
+print(country_tuple.index(countries))
