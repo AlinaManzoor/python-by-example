64a8fa6868a6ab04049a3ceabde4de68cfd3a033
AlinaManzoor <alina83318@gmail.com>
2025-01-13 17:11:27 +0500

    refactor: restructue files

---
+++ 51-draw-Square.py
@@ -0,0 +1,7 @@
+import turtle
+
+for i in range(0,4):           # 4 show the edges or corner 
+   step = turtle.forward(200) # show the length or steps
+   right = turtle.right(90)  # show the right angle
+
+turtle.exitonclick()        # show the stay duration 
