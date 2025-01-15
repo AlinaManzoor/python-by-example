c74b86077f1aff644c7abe1dee4572edeade070a
AlinaManzoor <alina83318@gmail.com>
2025-01-15 09:29:43 +0500

    refactor: restructue files

---
+++ 57-Display-country-name-by-index.py
@@ -0,0 +1,6 @@
+country_tuple = ("Pakistan" , "India" , "Afghanistan" , "Bangladesh" , "China")
+
+print(country_tuple)
+
+index = int(input("enter the number from (0-4): " , ))
+print((country_tuple[index]))
