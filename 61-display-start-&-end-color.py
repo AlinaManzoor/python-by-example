40042b3a6ccb02eec0d89913a9a4ad2e39d0e045
AlinaManzoor <alina83318@gmail.com>
2025-01-15 12:00:30 +0500

    refactor: restructue files

--- 61-display-start-&-end-color.py
+++ 61-display-start-&-end-color.py
@@ -3,7 +3,5 @@ color_list = ["Red"," Blue"," Green"," Yellow"," Purple","Orange","Pink","Black"
 start_no = int(input("Enter the a starting number between 0 and 4 : " , ))
 end_no = int(input("Enter the a ending number between 5 and 9 : " , ))
 
-"""index1 = color_list.index(start_no)
-index2 = color_list.index(end_no)"""
 print(color_list[start_no:end_no])
 
