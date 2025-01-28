b3940ff72c2c30aacec0a3d719b1f3d8d92e184e
AlinaManzoor <alina83318@gmail.com>
2025-01-28 19:01:17 +0500

    refactor: restructue files

---
+++ 84-Dictionary-input-alternation.py
@@ -0,0 +1,8 @@
+info = {"John" : {"N" : 3056, "S" : 8463 , "E" : 8441 , "W" : 2694} ,
+        "Tom" : {"N" : 9834, "S" : 7623 , "E" : 7352 , "W" : 7524} , 
+        "Anne" : { "N": 8743 , "S" : 9823 , "E" : 5629 , "w" : 9878}}
+
+nam = input("Enter the name : ",)
+region = input("Enter the region : " , )
+
+print(sales[nam][region])
