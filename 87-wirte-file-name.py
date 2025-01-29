0c6e849c668c68cd63619e5d3659611b3a890686
AlinaManzoor <alina83318@gmail.com>
2025-01-29 18:00:40 +0500

    refactor: restructue files

---
+++ 87-wirte-file-name.py
@@ -0,0 +1,11 @@
+file = open("Name.txt" , "w")
+
+file.write("Manzoor Abdul Gani\n")
+file.write("Nasreen Isque ali\n")
+file.write("Danish Manzoor\n")
+file.write("Aiman Manzoor\n")
+file.write("Daniyal Manzoor\n")
+file.write("Dawar Manzoor\n")
+file.write("Alina Manzoor\n")
+
+file.close()
